19d25ed7b6457ab7c19a68c1a8aadb91e2e6fd09
Oleg <olan-ol@yandex.ru>
2016-01-02 21:01:08 +0300

    Добавим процесс users в список контроля нашего супервизора

--- src/go2erlang_sup.erl
+++ src/go2erlang_sup.erl
@@ -8,5 +8,6 @@ start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	Procs = [],
-	{ok, {{one_for_one, 1, 5}, Procs}}.
+	Procs = [ {users, {users, start_link, []},
+				permanent, 5000, worker, [users]}],
+	{ok, {{one_for_one, 10, 10}, Procs}}.
